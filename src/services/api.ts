96dc2aff7a415bf55752bdb61b1b4006d59127aa
EgosPWD <egossolus@gmail.com>
2025-05-18 15:09:59 -0400

    select-cards

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,25 @@
 export async function createRoom(): Promise<string> {
   const res = await fetch("http://localhost:8000/room/create");
-  if (!res.ok) throw new Error("No se pudo crear la sala");
+  if (!res.ok) throw new Error("No se pudo creaar la sala");
   const data = await res.json();
   console.log(data.room_id);
   return data.room_id;
 }
+
+export async function SelectBlackCards(): Promise<string[]> {
+  const res = await fetch("http://localhost:8000/room/select_black-cards");
+  if (!res.ok) throw new Error("No se pudo seleccionar las cartas negras");
+  const data = await res.json();
+  console.log(data.black_cards);
+  return data.black_cards;
+
+
+}
+
+export async function SelectWhiteCards(): Promise<string[]> {
+  const res = await fetch("http://localhost:8000/room/select_white-cards");
+  if (!res.ok) throw new Error("No se pudo seleccionar las cartas blancas");
+  const data = await res.json();
+  console.log(data.white_cards);
+  return data.white_cards;
+} 
